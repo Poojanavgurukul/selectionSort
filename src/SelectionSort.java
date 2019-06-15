90c48c58ea18a72ab82283f97c9d4ea033415f52
Poojanavgurukul <Pooja17@navgurukul.org>
2019-06-16 04:01:48 +0530

    Refactor.In genre

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,24 +1,25 @@
 public class SelectionSort {
     public static void main(String[] args) {
-        int arr[] = {64,25,12,22,11};
-        selectionSort(arr);
+       Integer[] arr = {64,25,12,22,11};
+       selectionSort(arr);
         for (int i = 0; i <arr.length ; i++) {
             System.out.print(arr[i]+" ");
         }
     }
 
 
-    public static void selectionSort(int []numberList){
+    public static <T extends Comparable<T>>T[] selectionSort(T[]numberList){
         for (int i = 0; i <numberList.length-1 ; i++) {
             int mid=i;
             for (int j = i+1; j <numberList.length ; j++) {
-                if (numberList[j]<numberList[mid]){
+                if (numberList[j].compareTo(numberList[mid])<0){
                     mid=j;
                 }
             }
-            int swap=numberList[mid];
+            T swap=numberList[mid];
             numberList[mid]=numberList[i];
             numberList[i]=swap;
         }
+        return numberList;
     }
 }
